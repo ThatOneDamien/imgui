9f8b73aef5af3592d6e1ed012dcd412a48618baa
Damien Wilson <djwilson7704@gmail.com>
2023-05-19 17:00:48 -0500

    Added premake file to docking and changed the .gitignore

---
+++ premake5.lua
@@ -0,0 +1,48 @@
+project "imgui"
+	kind "StaticLib"
+	language "C"
+
+	targetdir ("bin/" .. outdir .. "/%{prj.name}")
+	objdir ("bin/int/" .. outdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_truetype.h",
+        "imstb_textedit.h",
+        "imgui_demo.cpp",
+        "imgui_tables.cpp"
+	}
+
+	filter "system:linux"
+		pic "on"
+
+	filter "system:macosx"
+		pic "on"
+
+	filter "system:windows"
+		systemversion "latest"
+        cppdialect "C++17"
+		staticruntime "on"
+
+    filter {"system:windows", "configurations:Release"}
+        buildoptions "/MT"
+		
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+		symbols "off"
