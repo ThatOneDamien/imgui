8f4a64380ee5902a524e8dd3ca0bc68cfc91e5f7
Damien Wilson <damienjw2104@gmail.com>
2024-06-28 20:01:38 -0500

    Removal of all Makefiles, and vsproject files

--- premake5.lua
+++ premake5.lua
@@ -1,15 +1,15 @@
 project "ImGui"
-	kind "StaticLib"
-	language "C++"
+    kind "StaticLib"
+    language "C++"
     cppdialect "C++17"
     staticruntime "on"
 
-	targetdir ("%{wks.location}/bin/" .. outdir .. "/%{prj.name}")
-	objdir ("%{wks.location}/bin/int/" .. outdir .. "/%{prj.name}")
+    targetdir ("%{wks.location}/bin/" .. outdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin/int/" .. outdir .. "/%{prj.name}")
 
-	files
-	{
-		"imconfig.h",
+    files
+    {
+        "imconfig.h",
         "imgui.h",
         "imgui.cpp",
         "imgui_draw.cpp",
@@ -20,21 +20,21 @@ project "ImGui"
         "imstb_textedit.h",
         "imgui_demo.cpp",
         "imgui_tables.cpp"
-	}
+    }
 
-	filter "system:linux"
-		pic "on"
+    filter "system:linux"
+        pic "on"
 
-	filter "system:macosx"
-		pic "on"
+    filter "system:macosx"
+        pic "on"
 
-	filter "system:windows"
-		systemversion "latest"
-		
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
+    filter "system:windows"
+        systemversion "latest"
+        
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
